fb53af278e9881970cff3ca560518a18790b0f4e
tao <sky_tao163@163.com>
2019-05-07 16:41:46 +0800

    提交

---
+++ SKYPodTestDemo/SKYPodTestDemo/SKYTwoViewController.h
@@ -0,0 +1,17 @@
+//
+//  SKYTwoViewController.h
+//  SKYPodTestDemo
+//
+//  Created by tao on 2019/5/7.
+//  Copyright © 2019 tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKYTwoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SKYPodTestDemo/SKYPodTestDemo/SKYTwoViewController.m
@@ -0,0 +1,32 @@
+//
+//  SKYTwoViewController.m
+//  SKYPodTestDemo
+//
+//  Created by tao on 2019/5/7.
+//  Copyright © 2019 tao. All rights reserved.
+//
+
+#import "SKYTwoViewController.h"
+
+@interface SKYTwoViewController ()
+
+@end
+
+@implementation SKYTwoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
