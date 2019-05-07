3f6be61b692a52a03f508fb7b1046c2d80ca96a2
tao <15236281882@163.com>
2019-05-07 10:27:26 +0800

    初始化项目

---
+++ SKYPodTestDemo/SKYPodTestDemo/SKYPodTestDemo.h
@@ -0,0 +1,19 @@
+//
+//  SKYPodTestDemo.h
+//  SKYPodTestDemo
+//
+//  Created by tao on 2019/5/7.
+//  Copyright © 2019 tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKYPodTestDemo.
+FOUNDATION_EXPORT double SKYPodTestDemoVersionNumber;
+
+//! Project version string for SKYPodTestDemo.
+FOUNDATION_EXPORT const unsigned char SKYPodTestDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKYPodTestDemo/PublicHeader.h>
+
+
